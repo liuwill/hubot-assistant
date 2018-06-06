a02a26360a685db0f67bf0901d62d6a60162b6d4
liuwill <liuwei_will@qq.com>
2018-06-06 18:39:23 +0800

    support bearychat hubot

--- start.sh
+++ start.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+usage()
+{
+  echo "USAGE: $CALLER [-h] COMMANDS"
+  echo "       $CALLER [ --help ]"
+  echo "COMMANDS:"
+  echo "    slack    运行slack机器人"
+  echo "    bearychat     运行bearychat机器人"
+  echo "Run '$CALLER COMMAND --help' for more information on a command."
+  exit 1
+}
+
+if [ $# -ne 1 ] ; then
+  usage
+  exit 0
+fi
+
+if [[ $1 != "slack" && $1 != "bearychat" ]]
+then
+  usage
+  exit 0
+fi
+
 if [ ! -d "$HUBOT_SLACK_TOKEN" ]
 then
   if [ ! -f ".env" ]
@@ -7,11 +29,13 @@ then
     echo '找不到环境变量'
     exit 1
   else
-    source .env
+    for line in `cat .env`
+    do
+        config=(${line//,/ })
+        export ${config[0]}=${config[1]}
+        #export HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN
+    done
   fi
-
 fi
 
-export HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN
-
-exec bin/hubot -a slack
+exec bin/hubot -a $1
